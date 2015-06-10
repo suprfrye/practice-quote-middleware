d23bbb821d0d50037ea97f55eddca891b5da93f8
suprfrye <tara.c.frye@gmail.com>
2015-06-10 17:14:31 -0400

    Setup simple app response

--- spec/gervais_quote_spec.rb
+++ spec/gervais_quote_spec.rb
@@ -4,4 +4,10 @@ require_relative '../lib/gervais_quote'
 describe GervaisQuote do
   include Rack::Test::Methods
 
+  let(:app) do
+    lambda do |env|
+      [200, {'Content-Type' => 'text/html'}, ['This is the content of app.']]
+    end
+  end
+
 end
