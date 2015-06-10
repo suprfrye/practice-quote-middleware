cb8ea337dc8676f4d6b75726a3afd47cc332f46b
suprfrye <tara.c.frye@gmail.com>
2015-06-10 16:46:47 -0400

    Add GervaisQuote#call method

--- lib/gervais_quote.rb
+++ lib/gervais_quote.rb
@@ -2,4 +2,12 @@ class GervaisQuote
   def initialize(app)
     @app = app
   end
+
+  def call(env)
+    if env['PATH_INFO'] == '/quote'
+      [200, {'Content-Type' => 'text/plain'}, ['Ricky quote.']]
+    else
+      @app.call(env)
+    end
+  end
 end
